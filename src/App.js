8ae01ef1c3acb8b55c7d1f90abc332c0d7d3ab03
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-07 23:59:11 -0500

    commit tree e15c107742e59002c2cc99e01d24bcc8b6659626

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+
+import React, {Component} from 'react'
+
+class App extends Component {
+  state={
+    data: []
+  }
+
+  async componentDidMount(){
+    let res = await fetch("http://api.open-notify.org/astros.json")
+    let data = await res.json()
+    this.setState({data})
+  }
+
+  render(){
+    return(
+      <div>
+        This is my App
+      </div>
+    )
+  }
+}
+
+export default App;
